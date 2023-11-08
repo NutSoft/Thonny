7c3aa5d79466ba1a6516fb297769dee0daf12775
Des Finkenzeller <nutsoft@hotmail.com>
2023-11-08 15:27:48 +0000

    removed commented out code

--- TrafficLights.py
+++ TrafficLights.py
@@ -1,20 +1,6 @@
 from machine import Pin
 import time
 
-#led1 = Pin(18, Pin.OUT)
-#led2 = Pin(19, Pin.OUT)
-#led3 = Pin(20, Pin.OUT)
-
-#led1.value(1)
-#led2.value(1)
-#led3.value(1)
-
-#time.sleep(5)
-
-#led1.value(0)
-#led2.value(0)
-#led3.value(0)
-
 ledRed = Pin(18, Pin.OUT)
 ledAmb = Pin(19, Pin.OUT)
 ledGrn = Pin(20, Pin.OUT)
@@ -31,7 +17,7 @@ while True:
     ledRed.value(1)
     ledAmb.value(1)
     ledGrn.value(0)
-    
+
     time.sleep(1)
     
     # Green

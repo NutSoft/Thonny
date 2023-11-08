19461ade6768556a802f6861434b03878e866060
Des Finkenzeller <nutsoft@hotmail.com>
2023-11-08 15:32:07 +0000

    simple beam break counter

---
+++ LightGate-2.py
@@ -0,0 +1,17 @@
+import machine
+import utime as time
+from machine import Pin
+
+# Simple beam break counter program for Raspberry Pi Pico
+
+beam_pin = Pin(15, Pin.IN, Pin.PULL_UP)
+old_beam_value = beam_pin.value()
+count = 0
+
+while True:
+    if old_beam_value != beam_pin.value():
+        old_beam_value = not old_beam_value
+        if old_beam_value == True:
+            count += 1
+            print(count)
+    time.sleep(0.1)

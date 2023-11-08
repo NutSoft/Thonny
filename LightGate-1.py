7d180199e32f96294d1fa657bb4fb2a20f135158
Des Finkenzeller <nutsoft@hotmail.com>
2023-11-08 15:28:14 +0000

    simple beam break code

---
+++ LightGate-1.py
@@ -0,0 +1,14 @@
+import machine
+import utime as time
+from machine import Pin
+
+# Super simple beam break program for Raspberry Pi Pico
+
+beam_pin = Pin(15, Pin.IN, Pin.PULL_UP)
+old_beam_value = beam_pin.value()
+
+while True:
+    if old_beam_value != beam_pin.value():
+        old_beam_value = not old_beam_value
+        print(old_beam_value)
+    time.sleep(0.1)

fa251412b39983f1d88b359f855fbff88892412c
Des Finkenzeller <nutsoft@hotmail.com>
2023-11-09 14:20:12 +0000

    drawing lines with SSD1306

---
+++ SSD1306/SSD1306-GFX2.py
@@ -0,0 +1,43 @@
+import random
+import time
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+# Quick and dirty test of my SSD1306 display
+
+i2c = I2C(0, sda = Pin(0), scl = Pin(1), freq = 400000)
+display = SSD1306_I2C(128, 64, i2c)
+
+# NutSoft Ltd
+# display.fill(0)
+# display.text("NutSoft Ltd", 0, 0)
+# display.text("NutSoft Ltd", 0, 16)
+# display.text("NutSoft Ltd", 0, 32)
+# display.text("NutSoft Ltd", 0, 48)
+# display.show()
+# time.sleep(5)
+
+# Draw the MicroPython logo and print some text
+display.fill(0)
+display.fill_rect(0, 16, 32, 32, 1)
+display.fill_rect(2, 18, 28, 28, 0)
+display.vline(9, 24, 22, 1)
+display.vline(16, 18, 22, 1)
+display.vline(23, 24, 22, 1)
+display.fill_rect(26, 40, 2, 4, 1)
+display.text('MicroPython', 40, 16, 1)
+display.text('SSD1306', 40, 28, 1)
+display.text('display 128x64', 40, 40, 1)
+display.show()
+time.sleep(5)
+
+# # Pixels
+# display.fill(0)
+# while True:
+#     xx = random.randint(0, 127)
+#     yy = random.randint(0, 63)
+#     display.pixel(xx, yy, 1)
+#     display.show()
+#     time.sleep(0.1)
+
+display.poweroff()
